d6582fd28631ce38089156d0dc9764865336b322
ptsheeran <ptsheeran@gmail.com>
2017-04-15 16:52:52 -0400

    DID STUFF

--- app/src/main/java/com/dvorak/ohiohackapp/MoreInfoActivity.java
+++ app/src/main/java/com/dvorak/ohiohackapp/MoreInfoActivity.java
@@ -2,6 +2,9 @@ package com.dvorak.ohiohackapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import static com.dvorak.ohiohackapp.R.id.textView;
 
 public class MoreInfoActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MoreInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more_info);
+        TextView waterHeating = (TextView) (findViewById(R.id.textView17));
+        TextView airCoolingHeating = (TextView)(findViewById(R.id.textView15));
+
+        int rate = 0;
+        waterHeating.setText("Water Heating: 14.46 kWh / $" + 14.46 * rate);
+        airCoolingHeating.setText("Air Heating and Cooling: 7.75kWh / $" + 7.75 * rate);
     }
 }
